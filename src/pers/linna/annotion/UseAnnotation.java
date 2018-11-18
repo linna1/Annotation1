86af08e304e264de6037f7c82e9b561a78ac4040
linna <1091109482@qq.com>
2018-11-18 23:50:30 +0800

    first commit

---
+++ src/pers/linna/annotion/UseAnnotation.java
@@ -0,0 +1,30 @@
+package pers.linna.annotion;
+
+public class UseAnnotation {
+
+	@JianCha
+    public void suanShu(){
+        System.out.println("1234567890");
+    }
+	@JianCha
+    public void jiafa(){
+        System.out.println("1+1="+1+1);
+    }
+	@JianCha
+    public void jiefa(){
+        System.out.println("1-1="+(1-1));
+    }
+	@JianCha
+    public void chengfa(){
+        System.out.println("3 x 5="+ 3*5);
+    }
+	@JianCha
+    public void chufa(){
+        System.out.println("6 / 0="+ 6 / 0);
+    }
+
+    public void ziwojieshao(){
+        System.out.println("我写的程序没有 bug!");
+    }
+
+}
